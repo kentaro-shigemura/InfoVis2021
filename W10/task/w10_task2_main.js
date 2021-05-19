f88e25c159fb103b12fcd34f82454e60a6c78e2b
重村健太朗 <ken@shigemurakentarounoMacBook-Pro.local>
2021-05-19 16:25:48 +0900

    Initial commit

--- W10/task/w10_task2_main.js
+++ W10/task/w10_task2_main.js
@@ -1,4 +1,4 @@
-d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
+d3.csv("https://kentaro-shigemura.github.io/InfoVis2021/W10/task/data6.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
@@ -6,7 +6,7 @@ d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: {top:10, right:10, bottom:20, left:10}
+            margin: {top:40, right:40, bottom:50, left:50}
         };
 
         const scatter_plot = new ScatterPlot( config, data );
@@ -23,7 +23,7 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            margin: config.margin || {top:40, right:40, bottom:50, left:50}
         }
         this.data = data;
         this.init();
@@ -40,30 +40,39 @@ class ScatterPlot {
             .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_width2 = self.config.margin.left + self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         self.xscale = d3.scaleLinear()
             .range( [0, self.inner_width] );
 
         self.yscale = d3.scaleLinear()
-            .range( [0, self.inner_height] );
+            .range( [self.inner_height, 0] );
 
         self.xaxis = d3.axisBottom( self.xscale )
             .ticks(6);
 
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(6);
+
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g')
+            .attr('transform', `translate( 0 , 0 )`);
+
+        self.text_group = self.svg.append('g');
     }
 
     update() {
         let self = this;
 
-        const xmin = d3.min( self.data, d => d.x );
-        const xmax = d3.max( self.data, d => d.x );
+        const xmin = d3.min( self.data, d => d.x ) - 1;
+        const xmax = d3.max( self.data, d => d.x ) + 1;
         self.xscale.domain( [xmin, xmax] );
 
-        const ymin = d3.min( self.data, d => d.y );
-        const ymax = d3.max( self.data, d => d.y );
+        const ymin = d3.min( self.data, d => d.y ) - 5;
+        const ymax = d3.max( self.data, d => d.y ) + 5;
         self.yscale.domain( [ymin, ymax] );
 
         self.render();
@@ -81,6 +90,38 @@ class ScatterPlot {
             .attr("r", d => d.r );
 
         self.xaxis_group
-            .call( self.xaxis );
+            .call( self.xaxis )
+            .append("text")
+            .attr("fill", "black")
+            .attr("x", (self.config.width  ) / 3 )
+            .attr("y", 35)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "10pt")
+            .attr("font-weight", "middle")
+            .text("month");
+
+        self.yaxis_group
+            .call( self.yaxis )
+            .append("text")
+            .attr("fill", "black")
+            .attr("text-anchor", "middle")
+            .attr("x", -(self.config.width  ) / 3 )
+            .attr("y", -35)
+            .attr("transform", "rotate(-90)")
+            .attr("font-weight", "middle")
+            .attr("font-size", "10pt")
+            .text("temperature");
+
+
+        self.text_group
+             .append("text")
+             .attr("fill", "black")
+             .attr("text-anchor", "middle")
+             .attr("x", (self.config.width ) / 2 )
+             .attr("y", 30)
+             .attr("font-weight", "middle")
+             .attr("font-size", "10pt")
+             .text("Monthly average temperature in Kobe");
+
     }
 }
