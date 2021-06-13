3b1aa928605dffb0e83b9ac5b3223c600ed02fe8
重村健太朗 <ken@shigemurakentarounoMacBook-Pro.local>
2021-06-14 01:36:12 +0900

    Initial commit

--- FinalTask/Task/final_task_main.js
+++ FinalTask/Task/final_task_main.js
@@ -19,16 +19,16 @@ d3.csv("https://kentaro-shigemura.github.io/InfoVis2021/FinalTask/convini_data.c
 
         bar_Chart = new BarChart2( {
             parent: '#drawing_region_barchart2',
-            width: 500,
-            height: 500,
+            width: 1000,
+            height: 1000,
             margin: {top:10, right:10, bottom:50, left:60},
         }, input_data );
         bar_Chart.update(filter);
 
         bar_chart = new BarChart( {
             parent: '#drawing_region_barchart',
-            width: 500,
-            height: 500,
+            width: 1000,
+            height: 1000,
             margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'Total number of convenience stores in Japan',
         }, input_data );
